9a2da1a8b52525e737c4ae4cf4b7191c6dfd3106
ahmadthegoat <ahcon1@morgan.edu>
2020-07-28 21:05:36 -0400

    finished

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,28 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem03 {
-}
+    public String checkValidity(Integer [] integerarray){
+        Integer [] sortedintegerarray = Arrays.copyOf(integerarray, integerarray.length);
+        Arrays.sort(sortedintegerarray);
+        int count =0;
+        for(int i=0; i<sortedintegerarray.length-1; i++){
+            if(Math.abs(sortedintegerarray[i] - integerarray[i]) > 2){
+                return "Too Chaotic";
+            }
+            else{
+                if(Math.abs(sortedintegerarray[i] - integerarray[i]) == 2){
+                    count+=2;
+                }
+                else if(Math.abs(sortedintegerarray[i] - integerarray[i]) == 1){
+                    count++;
+                }
+            }
+        }
+        if(count % 2 != 0) {
+            int result = (int)Math.round(count/2.0);
+            return String.valueOf(result);
+        }
+        return String.valueOf(count);
+}}
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,60 @@
 package com.stayready;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class Problem03Test {
+    Problem03 problem03 = new Problem03();
+
+    @Test
+    public void checkvaliditytest(){
+        Integer[] given = new Integer[]{2,1,5,3,4};
+        String result = problem03.checkValidity(given);
+        String expected = "3";
+
+        Assert.assertEquals(result,expected);
+
+    }
+
+    @Test
+    public void checkvaliditytest1(){
+        Integer[] given = new Integer[]{2,5,1,3,4};
+        String result = problem03.checkValidity(given);
+        String expected = "Too Chaotic";
+
+        Assert.assertEquals(result,expected);
+
+    }
+
+    @Test
+    public void checkvaliditytest2(){
+        Integer[] given = new Integer[]{2,1,5,4,3};
+        String result = problem03.checkValidity(given);
+        String expected = "4";
+
+        Assert.assertEquals(result,expected);
+
+    }
+
+
+    @Test
+    public void checkvaliditytest3(){
+        Integer[] given = new Integer[]{1,2,3,4,5};
+        String result = problem03.checkValidity(given);
+        String expected = "0";
+
+        Assert.assertEquals(result,expected);
+
+    }
+
+    @Test
+    public void checkvaliditytest4(){
+        Integer[] given = new Integer[]{2,1,3,4,5};
+        String result = problem03.checkValidity(given);
+        String expected = "2";
+
+        Assert.assertEquals(result,expected);
+
+    }
 }
+
